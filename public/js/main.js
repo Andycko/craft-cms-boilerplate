344ed5c81ea1a42f47065ebe8e784138a055fa75
Andrej Szalma <andrej@fortytwo.studio>
2021-04-06 15:37:58 +0100

    garbage collection and commenting

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
+// Initialize jquery
 const $ = require('jquery');
+
+// Import bootstrap js
 import '../../node_modules/bootstrap/dist/js/bootstrap';
+
+// Import custom js
 import './components/nav';
