343a8b3aaa4281deedddd3ba2ee05ff774d34bd9
Andrej Szalma <andy.szalma@gmail.com>
2022-04-05 16:39:25 +0200

    fixed nav to work with unlimited layers of dropdowns

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -1,5 +1,39 @@
 import $ from 'jquery';
 
+/**
+ * Finds a topmost parent element with a selector provided of the provided element
+ * @param {Node} element
+ * @param {string} selector
+ * @returns {Node}
+ */
+const findTopParentWithSelector = (element, selector) => {
+    let parent = element
+    while (parent.parentElement.closest(selector))
+        parent = parent.parentElement.closest(selector)
+    return parent
+}
+
+/**
+ * Check if an element is out of the viewport
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}  elem The element to check
+ * @return {Object}     A set of booleans for each side of the element
+ */
+const isOutOfViewport = function (elem) {
+    // Get element's bounding
+    const bounding = elem.getBoundingClientRect();
+    // Check if it's out of the viewport on each side
+    const out = {};
+    out.top = bounding.top < 0;
+    out.left = bounding.left < 0;
+    out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+    out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+    out.any = out.top || out.left || out.bottom || out.right;
+    out.all = out.top && out.left && out.bottom && out.right;
+
+    return out;
+};
+
 $(function () {
     const nav_hamburger = document.querySelector('#mobile-navbar-toggle');
     const mobile_nav = document.querySelector('#mobile-navbar-links');
@@ -8,4 +42,12 @@ $(function () {
         mobile_nav.classList.toggle('active');
         document.querySelector('html').classList.toggle('no-scroll')
     };
+
+    // check dropdowns out of bounds
+    const dropdowns = document.querySelectorAll('.sub-menu')
+    dropdowns.forEach((el) => {
+        if (isOutOfViewport(el).any)
+            findTopParentWithSelector(el, '.sub-menu').classList.add('left')
+    })
+
 });
