06750d4de0bc150da53bbf4dbfc7458ce52c3a03
Ross Thomson <rossthomson89@googlemail.com>
2023-04-20 12:08:10 +0100

    added imagesLoaded for imageSlider

--- src/js/components/imageSlider.js
+++ src/js/components/imageSlider.js
@@ -1,10 +1,15 @@
 import $ from 'jquery';
 import Flickity from 'flickity';
+require('flickity-imagesloaded');
 
 $(function(){
     if (document.querySelector('.imageSlider-block')) {
         const flkty = new Flickity('#image-slider', {
-            autoPlay: true
+            autoPlay: true,
+            cellAlign: 'left',
+            setGallerySize: true,
+            adaptiveHeight: true,
+            imagesLoaded: true
         });
     };
 });
