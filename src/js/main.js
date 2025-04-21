bbeee768a2d97301bfda42bfea842b51c94308ac
Ross Thomson <rossthomson89@googlemail.com>
2025-04-21 17:25:39 +0100

    imported bootstrap js & accordion fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,8 @@ import "@/css/style.css";
 // Import our custom CSS (pulls in bootstrap from /src/scss/styles.scss)
 import '../scss/styles.scss'
 
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 // Import custom js
 import './components/nav.js'; 
